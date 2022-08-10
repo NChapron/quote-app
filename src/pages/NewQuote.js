ecac7c2386501cda78a4fd2335aabfabf1a10351
Nicolas <nicolas.chapron@uleis.co.uk>
2022-08-10 15:07:14 +0100

    implementing programmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,11 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const AddQuoteHandler = (quoteData) => {
+    history.push("/quotes");
     console.log(quoteData);
   };
 
